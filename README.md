a71ce7fa85e696385a9f8b3c7c0a25588d94da57
djinncoyote <runlikecoyote@yahoo.com>
2019-03-24 19:31:02 -0400

    Updated script to allow flexible dates.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # PowershellLinkPoster
 Powershell Link Poster - Pinboard to Dreamwidth
 
-Note: This runs under PowerShell, which comes as default in Windows 10, and is available for Mac and Linux.  It uses the Dreamwidth "Post by Email" functionality.
+Note: This script uses the Dreamwidth "Post by Email" functionality.  It runs under PowerShell, which comes as default in Windows 10.  
+The script may not work for PowerShell running on MacOS or Linux, due to its use of a system environment variable.
 
 Run with a command-line like:
 
@@ -15,9 +16,13 @@ i.e. `postlinks.ps1 -pinboardUser andrewducker -emailFrom andrew@ducker.org.uk -
 
 If you're inside a corporate firewall then you will probably need to set the ProxyCredentials parameter.  And unless you have external SMTP access you'll need an internal mail server (use the "smtpServer" parameter).
 
-And you can change the posting time from midday by overriding the "linksEndTime" parameter like so:
+The first time the script is run, it will fetch all links that are present on your RSS feed - Pinboard keeps up to 70 of your last pinned links in the feed.
+The script will also set an environment variable to store the date and time that the script was last run.
 
-`-linksEndTime = (get-date -Minute 0 -Second 0 -Hour 8 -Millisecond 0)`
-which will post links for the 24 hours preceding 8am this morning.
+For subsequent runs, the script will fetch all links posted since the last time the script was run, and will update the environment variable each time.
+
+You can use the "dateFrom" and/or "dateTo" parameters to override which links are fetched (when "dateTo" is used, the environment variable will be set to that value):
+
+`postlinks.ps1 -pinboardUser andrewducker -emailFrom andrew@ducker.org.uk -emailTo andrewducker+1234@post.dreamwidth.org` -dateFrom 2019-03-20T12:00:00 -dateTo 2019-03-22T12:00:00
 
 All suggestions/merge requests gratefully received.
--- postlinks.ps1
+++ postlinks.ps1
@@ -1,13 +1,36 @@
 [CmdletBinding()]
 param(
 	[string]$pinboardUser = "",
-	[DateTime]$linksEndTime = (get-date -Minute 0 -Second 0 -Hour 12 -Millisecond 0),
+	[string]$dateFrom = "",
+	[string]$dateTo = "",
 	[string]$emailFrom = "",
-	[string]$emailTo = '',
+	[string]$emailTo = "",
 	[pscredential]$proxyCredentials,
 	[string]$smtpServer = "va-mail01.dreamwidth.org",
 	[switch]$TestMode
 )
+
+$envVarName = "LINKPOSTER_TIMESTAMP_$pinboardUser"
+
+if($dateFrom.Equals("")){
+	$dateFrom = [Environment]::GetEnvironmentVariable($envVarName, "User")
+
+	if(!$dateFrom.Equals("")){
+		Write-Verbose "Environment variable was found: $dateFrom"
+	}
+
+	$startDateTime = [DateTime]::ParseExact($dateFrom, "yyyy-MM-ddTHH:mm:ss", $null)
+}
+
+if($dateTo.Equals("")){
+	# Use the current date and time
+	$endDateTime = Get-Date
+	$dateTo = Get-Date -Date $endDateTime -Format "yyyy-MM-ddTHH:mm:ss"
+}
+else{
+	$endDateTime = [DateTime]::ParseExact($dateTo, "yyyy-MM-ddTHH:mm:ss", $null)
+}
+
 
 $wc = New-Object System.Net.WebClient
 if($proxyCredentials){
@@ -15,27 +38,27 @@ if($proxyCredentials){
 	$wc.Proxy.Credentials = $proxyCredentials
 }
 
-
 $pinboardUrl = "https://feeds.pinboard.in/rss/u:$pinboardUser/"
 Write-Verbose "Fetching from $pinboardUrl"
 [xml]$feed = $wc.DownloadString($pinboardUrl)
 
+# $feed.rdf.item.count doesn't work right; it returns 1 when there are no items because XmlElement has an "Item" property.
+$itemNodes = $feed.GetElementsByTagName("item")
 
-Write-Verbose "Feed has $($feed.rdf.item.count) entries"
+Write-Verbose "Feed has $($itemNodes.count) entries"
 
-$items = $feed.rdf.item | ? {[DateTime]::Parse($_.date) -gt $linksEndTime.AddDays(-1)}| ? {[DateTime]::Parse($_.date) -LE $linksEndTime} | sort {[DateTime]::Parse($_.date)}
+if($dateFrom.Equals("")){
+	Write-Verbose "Checking for links posted before: $dateTo"
 
-$itemCount = 0
-if($items){
-	if($items.count){
-		$itemCount = $items.Count
-	}
-	else{
-		$itemCount = 1
-	}
+	$items = $itemNodes | ? {[DateTime]::Parse($_.date) -LE $endDateTime} | sort {[DateTime]::Parse($_.date)}
+}
+else {
+	Write-Verbose "Checking for links posted between: $dateFrom and $dateTo"
+
+	$items = $itemNodes | ? {[DateTime]::Parse($_.date) -gt $startDateTime} | ? {[DateTime]::Parse($_.date) -LE $endDateTime} | sort {[DateTime]::Parse($_.date)}
 }
 
-Write-Verbose "$itemCount items selected"
+Write-Verbose "$($items.count) items selected"
 
 if($items){
 	$tags = @()
@@ -58,7 +81,7 @@ if($items){
 	}
 	$output += "</dl>"
 	
-	$output += "`n`n--`n`nDeletetionTrigger"
+	$output += "`n`n--`n`nDeletionTrigger"
 	
 	if($tags){
 		$tags += "links"
@@ -66,7 +89,7 @@ if($items){
 		$output = $tagsHeader + "`n`n" + $output
 	}
 
-	$subjectLink = "Interesting Links for $($linksEndTime.ToString("dd-MM-yyyy"))"
+	$subjectLink = "Interesting Links for $($endDateTime.ToString("dd-MM-yyyy"))"
 
 	$output = $output -replace "‘|’","'"
 	$output = $output -replace '–',"-"
@@ -80,3 +103,7 @@ if($items){
 	}
 }
 
+if(!$TestMode){
+	[Environment]::SetEnvironmentVariable($envVarName, $dateTo, "User")
+	Write-Verbose "Updated environment variable to: $dateTo"
+}
